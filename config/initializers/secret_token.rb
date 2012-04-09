2c1e4244e042ab5c3c82d07b07fa894a7c0372f7
Xing Zhou <dekom.dev@gmail.com>
2012-04-09 20:10:10 +0000

    Initial commit after some editing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NlaWebsite2::Application.config.secret_token = '3bd275e7168ca94b3981d1efcd7118df14ad469e2324c91cf762226d579426f9d8e5bcdf287b54c4fab552a9f90e5597b2bd78675a3057d13f6027920286d849'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NlaWebsite2::Application.config.session_store :cookie_store, key: '_nla-website-2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NlaWebsite2::Application.config.session_store :active_record_store
